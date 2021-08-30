c0aee3858674468dd6dbc738dfc44acdf90a008d
uchuk <55624380+uchu-kitagawa@users.noreply.github.com>
2021-08-31 00:41:22 +0900

    add TODO App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // li作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // button作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    const p = document.createElement("p");
+    p.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    const li = document.createElement("li");
+
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+    li.appendChild(addTarget);
+    document.getElementById("complete-list").appendChild(li);
+    console.log(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // div要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
